333f41821109ab37739ad74b31b55832aee4f779
Jorge Cróquer <jacroquer.18@est.ucab.edu.ve>
2026-06-10 12:22:39 -0400

    feat(app): scaffold flutter application con melos
    - Agrega melos.yaml y pubspec.yaml raiz
    - Crea flutter app en apps/cuentaria_app
    - Configura placeholder_screen para MVP
    - Agrega widget test basico para app boots

---
+++ apps/cuentaria_app/README.md
@@ -0,0 +1,3 @@
+# cuentaria_app
+
+A new Flutter project.
---
+++ apps/cuentaria_app/android/app/src/main/kotlin/com/example/cuentaria_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cuentaria_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
